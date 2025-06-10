8a11427b48b9ac5b8a20add0bc750be8f9494ed3
Nokita Kaze <admin@kanaria.ru>
2025-06-10 19:46:14 +0300

    * Correct ports in start.sh

--- openvpn-server-no-tls/start.sh
+++ openvpn-server-no-tls/start.sh
@@ -89,9 +89,9 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-# Allow UDP traffic on port 1194.
-iptables -A INPUT -i $ADAPTER -p udp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
-iptables -A OUTPUT -o $ADAPTER -p udp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
+# Allow UDP traffic on port 1195.
+iptables -A INPUT -i $ADAPTER -p udp -m state --state NEW,ESTABLISHED --dport 1195 -j ACCEPT
+iptables -A OUTPUT -o $ADAPTER -p udp -m state --state ESTABLISHED --sport 1195 -j ACCEPT
 
 # Allow traffic on the TUN interface.
 iptables -A INPUT -i tun0 -j ACCEPT
